ae5d7507b6ff9f20af7f884663ed52e7f975994e
Sergey_Tyutvin <tyutvi@gmail.com>
2022-06-05 16:18:15 +0300

    Задача №4 решена

---
+++ Task04/Program.cs
@@ -0,0 +1,17 @@
+﻿internal partial class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Введите первое число:");
+        int a = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите второе число:");
+        int b = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите третье число:");
+        int c = Convert.ToInt32(Console.ReadLine());
+        int max = a;
+        if (a < b) max = b;
+        if (b < c) max = c;
+        Console.WriteLine("Максимальное число:");
+        Console.Write(max);
+    }
+}
