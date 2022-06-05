77b3da1ff17ca22d4ddb8cb08c1d76baeb57c880
Sergey_Tyutvin <tyutvi@gmail.com>
2022-06-05 16:39:17 +0300

    Добавлено условие задачи в качестве комментария

--- Task04/Program.cs
+++ Task04/Program.cs
@@ -2,6 +2,7 @@
 {
     private static void Main(string[] args)
     {
+        //Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
         Console.WriteLine("Введите первое число:");
         int a = Convert.ToInt32(Console.ReadLine());
         Console.WriteLine("Введите второе число:");
