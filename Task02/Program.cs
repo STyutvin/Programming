d8ecb58720489666d1165cd4c6577d330b4aeab3
Sergey_Tyutvin <tyutvi@gmail.com>
2022-06-05 16:36:23 +0300

    Добавлено условие задачи в качестве комментария

--- Task02/Program.cs
+++ Task02/Program.cs
@@ -2,6 +2,7 @@
 {
     private static void Main(string[] args)
     {
+        //Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
         Console.WriteLine("Введите первое число:");
         int a = Convert.ToInt32(Console.ReadLine());
         Console.WriteLine("Введите второе число:");
