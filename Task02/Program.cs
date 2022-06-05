5478b05ba16b7a2714e0425046c2d686261be1fc
Sergey_Tyutvin <tyutvi@gmail.com>
2022-06-05 15:58:48 +0300

    Задача решена

--- Task02/Program.cs
+++ Task02/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Введите первое число:");
+        int a = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите второе число:");
+        int b = Convert.ToInt32(Console.ReadLine());
+        if (a > b)
+        {
+            Console.Write(a);
+            Console.Write(" больше ");
+            Console.Write(b);
+        }
+        else
+        {
+            Console.Write(a);
+            Console.Write(" меньше ");
+            Console.Write(b);
+        }
+    }
+}
