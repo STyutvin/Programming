07b0fcf93c6ca8ca67544f2271312670924325c3
Sergey_Tyutvin <tyutvi@gmail.com>
2022-06-05 17:17:44 +0300

    Задача решена

--- Task08/Program.cs
+++ Task08/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal partial class Program
+{
+    private static void Main(string[] args)
+    {
+        //Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+        Console.WriteLine("Введите число любое положительное число:");
+        int n = Convert.ToInt32(Console.ReadLine());
+        int b = 2;
+        Console.WriteLine("Вывод результата:");
+        if (n >= 2)
+        {
+            while (b <= n)
+            {
+                Console.WriteLine(b);
+                b = b + 2;
+            }
+        }
+        else
+        {
+            Console.WriteLine("Количество четных чисел равно 0.");
+        }
+    }
+}
