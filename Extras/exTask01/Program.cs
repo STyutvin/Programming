fd1dba455534da8efd6207048a3bc47ddf7abda3
Sergey_Tyutvin <tyutvi@gmail.com>
2022-06-06 21:50:47 +0300

    Ура! Сверхурочные!

---
+++ Extras/exTask01/Program.cs
@@ -0,0 +1,10 @@
+﻿//Пользователь вводит число N (N > 0). Программа должна вывести N единиц на экран.
+Console.WriteLine("Введите число:");
+int n = Convert.ToInt32(Console.ReadLine());
+int i=0;
+Console.WriteLine("Вывод результата:");
+while(i<n)
+	{
+	    Console.Write("1");
+	i=i+1;
+	}
