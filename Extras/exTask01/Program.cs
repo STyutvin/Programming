a3e645552e83131ac2cbf0b4d55c2058b1121873
Sergey_Tyutvin <tyutvi@gmail.com>
2022-06-06 21:53:50 +0300

    Приведение программы в надлежащий вид

--- Extras/exTask01/Program.cs
+++ Extras/exTask01/Program.cs
@@ -1,10 +1,16 @@
-﻿//Пользователь вводит число N (N > 0). Программа должна вывести N единиц на экран.
-Console.WriteLine("Введите число:");
-int n = Convert.ToInt32(Console.ReadLine());
-int i=0;
-Console.WriteLine("Вывод результата:");
-while(i<n)
-	{
-	    Console.Write("1");
-	i=i+1;
-	}
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Пользователь вводит число N (N > 0). Программа должна вывести N единиц на экран.
+        Console.WriteLine("Введите число:");
+        int n = Convert.ToInt32(Console.ReadLine());
+        int i = 0;
+        Console.WriteLine("Вывод результата:");
+        while (i < n)
+        {
+            Console.Write("1");
+            i = i + 1;
+        }
+    }
+}
