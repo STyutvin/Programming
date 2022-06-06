96d0fdd357d8964d83ccf610e361775439774e13
Sergey_Tyutvin <tyutvi@gmail.com>
2022-06-06 23:24:18 +0300

    Решение дополнительной задачи №4

---
+++ Extras/exTask04/Program.cs
@@ -0,0 +1,14 @@
+﻿// Дано N секунд. Вывести время в формате часы:минуты:секунды.
+Console.WriteLine("Введите время в секундах:");
+int time = Convert.ToInt32(Console.ReadLine());
+int hours;
+int minutes;
+int seconds;
+hours=time/3600;
+minutes=(time%3600)/60;
+seconds=(time%3600)%60;
+Console.Write(hours);
+Console.Write(":");
+Console.Write(minutes);
+Console.Write(":");
+Console.Write(seconds);
