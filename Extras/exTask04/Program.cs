3fda6f99909c8e83eaf0627ab6202b1c804cbf2e
Sergey_Tyutvin <tyutvi@gmail.com>
2022-06-06 23:31:40 +0300

    Приведение задачи №4 в надлежащий вид

--- Extras/exTask04/Program.cs
+++ Extras/exTask04/Program.cs
@@ -1,14 +1,20 @@
-﻿// Дано N секунд. Вывести время в формате часы:минуты:секунды.
-Console.WriteLine("Введите время в секундах:");
-int time = Convert.ToInt32(Console.ReadLine());
-int hours;
-int minutes;
-int seconds;
-hours=time/3600;
-minutes=(time%3600)/60;
-seconds=(time%3600)%60;
-Console.Write(hours);
-Console.Write(":");
-Console.Write(minutes);
-Console.Write(":");
-Console.Write(seconds);
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Дано N секунд. Вывести время в формате часы:минуты:секунды.
+        Console.WriteLine("Введите время в секундах:");
+        int time = Convert.ToInt32(Console.ReadLine());
+        int hours;
+        int minutes;
+        int seconds;
+        hours = time / 3600;
+        minutes = time % 3600 / 60;
+        seconds = time % 3600 % 60;
+        Console.Write(hours);
+        Console.Write(":");
+        Console.Write(minutes);
+        Console.Write(":");
+        Console.Write(seconds);
+    }
+}
